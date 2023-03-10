304fabd3773c331c9a5a497c7472cb375737f5f2
Enrique Prieto <enrique.neto@safetydocs.com.br>
2023-03-10 11:12:16 -0300

    chore: first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Alexandrita
+
+A public FrontEnd bootstrap by Prieto Tecnologia company.
+
+```
+npm run build
+```
+
+or
+
+```
+yarn build
+```
+
+to bundle your application
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : "style-loader";
+
+const config = {
+  entry: {
+    alexandrita: ['./src/alexandrita.scss']
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, "src")
+    }
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new MiniCssExtractPlugin());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
